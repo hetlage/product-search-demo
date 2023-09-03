c923192116a3e3282edd3249aa655c82bad1ecef
John Hetlage <anton@anton>
2023-09-03 14:26:47 -0500

    Setup Initial Flask App Factory Pattern

---
+++ backend/wsgi.py
@@ -0,0 +1,13 @@
+import os
+
+from app import create_app
+
+
+config_name = os.environ.get('APP_CONFIG', 'dev')
+if config_name == 'dev':
+    print('WARNING: RUNNING IN DEVELOPMENT MODE. SET APP_CONFIG=prod IF DEPLOYING.')
+
+application = create_app(config_name)
+
+if __name__ == '__main__':
+    application.run()
