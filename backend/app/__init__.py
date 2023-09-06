6b42cc9410e0e242cbe2b455e3b99aae175383c5
John Hetlage <gr8appskc@gmail.com>
2023-09-06 01:23:11 -0500

    Revert "Feature/frontend/initial frontend config"

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,16 +1,20 @@
 from flask import Flask, jsonify
+from flask_marshmallow import Marshmallow
 
 from config import config
 
+ma = Marshmallow()
 
-def create_app(config_name="dev"):
+def create_app(config_name='dev'):
     """App factory method for initializing flask extensions and registering
     blueprints."""
-    app = Flask(__name__)
-    app.config.from_object(config[config_name])
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_object(config[config_name]) 
+    ma.init_app(app)
 
     # a simple ping that says hello
-    @app.route("/ping")
+    @app.route('/ping')
     def ping():
         return jsonify({"message": "Hello, World"}), 200
 
--- frontend/jest.config.cjs
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    collectCoverageFrom: ['src/**/*.{js,jsx}'],
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-}
